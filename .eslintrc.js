e8ec87ac9bb4a675e9697f6c9154d85ad16c5e6e
Ludvig Lundgren <hello@ludviglundgren.se>
2018-05-21 07:06:07 +0200

    Add eslint config for linting

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": ["eslint-config-airbnb", "plugin:prettier/recommended"],
+    "env": {
+        "browser": true,
+    },
+    "rules": {
+        "react/jsx-filename-extension": [0],
+        "react/prop-types": [0]
+    },
+};
